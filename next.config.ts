b91018f9cf729c4b2f44ac00bcbccedacdf6399b
Emran <32483285+emranio@users.noreply.github.com>
2026-07-19 23:09:49 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,21 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: "/arraysubs/compare/yith-membership/",
+        destination:
+          "/deals/arraysubs/alternatives/yith-woocommerce-membership/",
+        statusCode: 301,
+      },
+      {
+        source: "/arraysubs/features/payment-gateways/",
+        destination: "/deals/arraysubs/features/payment-gateways/",
+        statusCode: 301,
+      },
+    ];
+  },
   images: {
     unoptimized: true,
   },
